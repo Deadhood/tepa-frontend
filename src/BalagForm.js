3b87e639a0ffccb38af6c97586caeb2b3f6a04ad
srsajjad <sajjadsr001@gmail.com>
2017-05-30 20:46:25 +0600

    added more fields except tables and add click-form

--- src/BalagForm.js
+++ src/BalagForm.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
 import Form from 'react-jsonschema-form'
-import { inject, observer } from 'mobx-react'
+import {
+  inject,
+  observer
+} from 'mobx-react'
 
 const schema = {
   title: 'টেপা',
@@ -162,6 +167,247 @@ const schema = {
       title: 'বিদ্যুতের উৎস',
       type: 'string',
       enum: ['পল্লী বিদ্যুৎ', 'পিডিবি', 'সৌর বিদ্যুৎ', 'বায়ো গ্যাস']
+    },
+    'agro-instrument': {
+      title: 'কৃষি যন্ত্রপাতি',
+      type: 'array',
+      items: {
+        type: 'string',
+        enum: ['ডিজেল চালিত মেশিন', 'ডিপ টিউবওয়েল', 'ট্রাক্টর', 'পাওয়ার ট্রিলার', 'বৈদ্যুতিক মটর', 'স্প্রে মেশিন', 'ধান মাড়াই মেশিন', 'ধান কাটা মেশিন', 'মাছ ধরার জাল', 'নৌকা', 'অন্যান্য']
+      },
+      uniqueItems: true
+    },
+
+    'land-percentage': {
+      title: 'জমির পরিমাণ শতাংশে',
+      type: 'object',
+
+      'properties': {
+        krishi: {
+          title: 'কৃষি',
+          type: 'string'
+        },
+        'bash-jhar': {
+          title: 'বাঁশঝার',
+          type: 'string'
+        },
+        pukur: {
+          title: 'পুকুর',
+          type: 'string'
+        },
+        onabadi: {
+          title: 'অনাবাদী',
+          type: 'string'
+        },
+        'bondhok-neoa': {
+          title: 'বন্ধক নেওয়া',
+          type: 'string'
+        },
+        'bondhok-deoa': {
+          title: 'বন্ধক দেওয়া',
+          type: 'string'
+        },
+        'dokhole-rakha': {
+          title: 'দখলে রাখা',
+          type: 'string'
+        },
+        onnanno: {
+          title: 'অন্যান্য',
+          type: 'string'
+        }
+      }
+    },
+    cattle: {
+      title: 'গবাদী পশুর তথ্য',
+      type: 'object',
+
+      'properties': {
+        goru: {
+          title: 'গরু',
+          type: 'number'
+        },
+        mohish: {
+          title: 'মহিষ',
+          type: 'number'
+        },
+        vera: {
+          title: 'ভেড়া',
+          type: 'number'
+        },
+        chagol: {
+          title: 'ছাগল',
+          type: 'number'
+        },
+        murgi: {
+          title: 'মুরগী',
+          type: 'number'
+        },
+        hash: {
+          title: 'হাঁস',
+          type: 'number'
+        },
+        onnanno: {
+          title: 'অন্যান্য',
+          type: 'number'
+        }
+      }
+    },
+    kitnashok: {
+      title: 'বীজ ও কীটনাশক',
+      type: 'object',
+
+      'properties': {
+        dokan: {
+          title: 'দোকান',
+          type: 'string'
+        },
+        bds: {
+          title: 'বিডিএস',
+          type: 'string'
+        },
+        dealer: {
+          title: 'অনুমোদিত ডিলার',
+          type: 'string'
+        },
+        onnanno: {
+          title: 'অন্যান্য',
+          type: 'string'
+        }
+      }
+    },
+    'car-info': {
+      title: 'মোটর গাড়ির তথ্য',
+      type: 'object',
+
+      'properties': {
+        'car-license': {
+          title: 'গাড়ির লাইসেন্স',
+          type: 'boolean'
+        },
+        'driving-license': {
+          title: 'ড্রাইভিং লাইসেন্স',
+          type: 'boolean'
+        },
+        'cars': {
+          title: 'মোটর গাড়ি সমূহ',
+          type: 'array',
+          items: {
+            type: 'string',
+            enum: ['মটর বাইক', 'মাইক্রোবাস',
+              'কার', 'জিপ',
+              'অটোবাইক', 'ট্রাক', 'অন্যান্য'
+            ]
+          },
+          uniqueItems: true
+        }
+      }
+    },
+    mamla: {
+      title: 'মামলা',
+      type: 'object',
+
+      properties: {
+        'deoani': {
+          title: 'দেওয়ানী মামলা',
+          type: 'boolean'
+        },
+        foujdari: {
+          title: 'ফৌজদারী মামলা',
+          type: 'boolean'
+        },
+        help: {
+          title: 'সহায়তা পেয়েছে',
+          type: 'boolean'
+        }
+      }
+    },
+    orphan: {
+      title: 'এতিম শিশু',
+      type: 'object',
+
+      properties: {
+        name: {
+          title: 'নাম',
+          type: 'string'
+        },
+        age: {
+          title: 'বয়স',
+          type: 'number'
+        },
+        'no-dad': {
+          title: 'পিতৃ হারা',
+          type: 'boolean'
+        },
+        'no-mom': {
+          title: 'মাতৃ হারা',
+          type: 'boolean'
+        },
+        accident: {
+          title: 'উক্ত পরিবারের দুর্ঘটনা জনিত মৃত্যু',
+          type: 'boolean'
+        },
+        'organ-damage': {
+          title: 'দুর্ঘটনায় অঙ্গহানি',
+          type: 'boolean'
+        }
+      }
+    },
+    maid: {
+      title: 'গৃহ কর্মী',
+      type: 'object',
+
+      properties: {
+        name: {
+          title: 'নাম',
+          type: 'string'
+        },
+        age: {
+          title: 'বয়স',
+          type: 'number'
+        },
+        address: {
+          title: 'ঠিকানা',
+          type: 'string'
+        },
+        gender: {
+          title: 'লিঙ্গ',
+          type: 'string',
+          enum: ['নারী', 'পুরুষ']
+        }
+
+      }
+    },
+    'kota-info': {
+      title: 'অন্যান্য',
+      type: 'object',
+
+      properties: {
+        'freedom-fighter': {
+          title: 'পরিবারে মুক্তিযোদ্ধা আছে',
+          type: 'boolean'
+        },
+        'upojati': {
+          title: 'উপজাতি',
+          type: 'boolean'
+        },
+        '0-to-15-child': {
+          title: '০-১৫ মাস পর্যন্ত বয়সী শিশু আছে',
+          type: 'boolean'
+        },
+        'tika': {
+          title: 'থাকলে টিকা নেয় কিনা',
+          type: 'boolean'
+        },
+        'is-voter': {
+          title: 'ইউনিয়নে	ভোটার অন্তভূক্ত',
+          type: 'boolean'
+        },
+        'how-many-votes': {
+          title: 'হলে ভোট সংখ্যা কত',
+          type: 'number'
+        }
+
+      }
     }
   }
 }
@@ -215,6 +461,14 @@ const uiSchema = {
       inline: true
     }
   },
+  maid: {
+    gender: {
+      'ui:widget': 'radio',
+      'ui:options': {
+        inline: true
+      }
+    }
+  },
   health: {
     'ui:widget': 'radio',
     'ui:options': {
@@ -232,21 +486,40 @@ const uiSchema = {
     'ui:options': {
       inline: true
     }
+  },
+  'agro-instrument': {
+    'ui:widget': 'checkboxes',
+    'ui:options': {
+      inline: true
+    }
+  },
+  'car-info':{
+    'cars': {
+    'ui:widget': 'checkboxes',
+    'ui:options': {
+      inline: true
+    }
+  }
   }
 }
 
 class BalagForm extends Component {
-  render () {
-    return (
-      <Form
-        schema={schema}
-        uiSchema={uiSchema}
-        onSubmit={this._handleSubmit.bind(this)}
+  render() {
+    return ( <
+      Form schema = {
+        schema
+      }
+      uiSchema = {
+        uiSchema
+      }
+      onSubmit = {
+        this._handleSubmit.bind(this)
+      }
       />
     )
   }
 
-  _handleSubmit (data) {
+  _handleSubmit(data) {
     this.props.dataStore.formData = data.formData
   }
 }
